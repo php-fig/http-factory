9d20efdc77999515729c2aaa91508238ac8e4214
Matthew Weier O'Phinney <matthew@zend.com>
2018-07-30 15:18:37 -0500

    Imports implementation from http-interop/http-factory
    Imports implementation, and makes the following changes:
    
    - `s/Interop/Psr/g`
    - `s/http-interop/psr/g`
    - Reference final, accepted specification

---
+++ src/RequestFactoryInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Psr\Http\Factory;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+interface RequestFactoryInterface
+{
+    /**
+     * Create a new request.
+     *
+     * @param string $method The HTTP method associated with the request.
+     * @param UriInterface|string $uri The URI associated with the request. If
+     *     the value is a string, the factory MUST create a UriInterface
+     *     instance based on it.
+     *
+     * @return RequestInterface
+     */
+    public function createRequest(string $method, $uri): RequestInterface;
+}
